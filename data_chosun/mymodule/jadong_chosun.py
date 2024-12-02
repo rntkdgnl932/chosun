c73a2b1a52b9c28a23653081645c8f4177274d18
coob <koosanghwi@gmail.com>
2024-12-03 02:40:09 +0900

    ver 1.0392

--- data_chosun/mymodule/jadong_chosun.py
+++ data_chosun/mymodule/jadong_chosun.py
@@ -38,7 +38,7 @@ def jadong_start(cla):
                 full_path = "c:\\my_games\\chosun\\data_chosun\\imgs\\jadong\\spot\\" + str(read_data) + ".PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(750, 340, 920, 380, cla, img, 0.8)
+                imgs_ = imgs_set_(750, 340, 920, 380, cla, img, 0.92)
                 if imgs_ is not None and imgs_ != False:
                     print("spot...", str(read_data), imgs_)
                     is_spot = True
