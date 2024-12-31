cbf5bba017fabb1c02a9fbeb6e03844660811fd6
coob <koosanghwi@gmail.com>
2025-01-01 03:36:50 +0900

    ver 1.046

--- data_chosun/mymodule/character_select_and_game_start.py
+++ data_chosun/mymodule/character_select_and_game_start.py
@@ -15,6 +15,14 @@ def game_start_screen(cla, character_id):
     from action_chosun import all_confirms, game_loading_check, game_loading
 
     try:
+        full_path = "c:\\my_games\\chosun\\data_chosun\\imgs\\check\\game_check\\continue_btn.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(650, 500, 760, 560, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            print("continue_btn", imgs_)
+            click_pos_reg(imgs_.x, imgs_.y, cla)
+            time.sleep(1)
 
         # 실수로 새 캐릭터 클릭시...
         full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\game_start\\create_btn.PNG"
